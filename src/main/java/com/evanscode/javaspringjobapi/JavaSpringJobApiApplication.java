b0ab1c2f778fce043b28ce2bfc82d50effc0b53e
Evans <pauliutsevans@gmail.com>
2024-05-19 19:09:41 +0300

    Day 1 - 1

---
+++ src/main/java/com/evanscode/javaspringjobapi/JavaSpringJobApiApplication.java
@@ -0,0 +1,13 @@
+package com.evanscode.javaspringjobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringJobApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evanscode/javaspringjobapi/JavaSpringJobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evanscode.javaspringjobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringJobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
